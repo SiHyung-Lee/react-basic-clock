da8ac953d5c94c42ab5800656cf230c2b437c4cb
sihyung-lee <buzzcatfish@gmail.com>
2019-12-24 22:49:30 +0900

    add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
 
-class Time extends Component {
+class CurrentDate extends Component {
     date = new Date();
     state = {
+        year: this.date.getFullYear(),
+        month: this.date.getMonth() + 1,
+        day: this.date.getDate(),
         hour: this.date.getHours(),
         minute: this.date.getMinutes(),
         second: this.date.getSeconds()
@@ -23,7 +26,8 @@ class Time extends Component {
     render() {
         return (
             <div>
-                {this.state.hour}:{this.state.minute}:{this.state.second}
+                <div>{this.state.hour}:{this.state.minute}:{this.state.second}</div>
+                <div>{this.state.year}.{this.state.month}.{this.state.day}</div>
             </div>
         )
     }
@@ -32,7 +36,7 @@ class Time extends Component {
 function App() {
     return (
         <div className="App">
-            <Time/>
+            <CurrentDate/>
         </div>
     );
 }
