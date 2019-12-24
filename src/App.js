984170f79fa7b274a366944c17fd749c289bc9a6
SiHyeongLee <buzzcatfish@gmail.com>
2019-12-24 13:46:49 +0900

    시간 데이터 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+class Time extends Component {
+    date = new Date();
+    state = {
+        hour: this.date.getHours(),
+        minute: this.date.getMinutes(),
+        second: this.date.getSeconds()
+    };
+
+    componentDidMount() {
+        setInterval(() => {
+            this.date = new Date();
+
+            this.setState({
+                hour: this.date.getHours(),
+                minute: this.date.getMinutes(),
+                second: this.date.getSeconds()
+            });
+        }, 1000);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.hour}:{this.state.minute}:{this.state.second}
+            </div>
+        )
+    }
+}
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Time/>
+        </div>
+    );
 }
 
 export default App;
